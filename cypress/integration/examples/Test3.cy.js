cebd07fb091a40852415849d0bcc717b0e0ff24a
macbook air <macbookair@macbooks-MacBook-Air.local>
2023-09-26 17:14:03 +0400

    course

---
+++ cypress/integration/examples/Test3.cy.js
@@ -0,0 +1,41 @@
+/// <reference types="Cypress" />
+ 
+describe('My Second Test Suite', function() 
+{
+ 
+it('My Second Test case',function() {
+ 
+ 
+cy.visit("https://rahulshettyacademy.com/AutomationPractice/")
+cy.get('#checkBoxOption1').check().should('be.checked').and('have.value', 'option1')
+
+cy.get('#checkBoxOption1').uncheck().should('not.be.checked')
+cy.get('input[type="checkbox"]').check(['option2', 'option3'])
+
+//static dropdown
+cy.get('select').select('option2').should('have.value', 'option2')
+
+//dynamic dropdowns
+cy.get('#autocomplete').type('ind')
+cy.get('.ui-menu-item div').each(($el, index, $list) =>{
+    if($el.text()==="India"){
+        cy.wrap($el).click()
+    }
+})
+
+cy.get('#autocomplete').should('have.value', 'India')
+cy.get('#displayed-text').should('be.visible')
+cy.get('#hide-textbox').click()
+cy.get('#displayed-text').should('not.be.visible')
+cy.get('#show-textbox').click()
+cy.get('#displayed-text').should('be.visible')
+
+//radio buttons
+cy.get('[value="radio1"]').check().should('be.checked')
+
+ 
+}  )
+ 
+ 
+ 
+}  )
---
+++ cypress/integration/examples/Test4.cy.js
@@ -0,0 +1,30 @@
+/// <reference types="Cypress" />
+ 
+describe('My Four Test Suite', function() 
+{
+ 
+it('My four Test case',function() {
+ 
+ 
+cy.visit("https://rahulshettyacademy.com/AutomationPractice/")
+
+cy.get('#alertbtn').click()
+cy.get('[value="Confirm"]').click()
+//window alert
+cy.on('window:alert', (str) => {
+    //Mocha
+    expect(str).to.equal('Hello , share this practice page and share your knowledge')
+})
+
+//windo confirm
+cy.on('window:confirm', (str) => {
+    //Mocha
+    expect(str).to.equal('Hello , Are you sure you want to confirm?')
+})
+
+ 
+}  )
+ 
+ 
+ 
+}  )
